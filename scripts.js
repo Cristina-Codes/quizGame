0d2159936b098f25bd165b4c60ef8f1769db772a
CristinaGrosser <cmgrosser@comcast.net>
2022-03-10 14:16:13 -0800

    Fixing a display answers problem

--- scripts.js
+++ scripts.js
@@ -81,18 +81,18 @@ document.getElementById("answers").onclick=function() {
 	if($(window).width() > 767) {
 		hide('answers');
 		hide('score');
-		// document.getElementById('resultsDisplay').innerHTML = "<table><tr><thead><td>Correct Answers</td><td>Your Answers</td></thead></tr>" + 
-		// "<tr><td><ol><li>World Wide Web</li><li>Octagon</li><li>Marie Curie</li><li>Tea</li><li>China</li><li>Volleyball</li></ol></td>" +
-		// "<td><ol><li>" + answersArray[0] + "</li><li>" + answersArray[1] + "</li><li>" + answersArray[2] + "</li><li>" + answersArray[3] + 
-		// "</li><li>" + answersArray[4] + "</li><li>" + answersArray[5] + "</li></ol></td></tr></table>"
+		document.getElementById('resultsDisplay').innerHTML = "<table><tr><thead><td>Correct Answers</td><td>Your Answers</td></thead></tr>" + 
+		"<tr><td><ol><li>World Wide Web</li><li>Octagon</li><li>Marie Curie</li><li>Tea</li><li>China</li><li>Volleyball</li></ol></td>" +
+		"<td><ol><li>" + answersArray[0] + "</li><li>" + answersArray[1] + "</li><li>" + answersArray[2] + "</li><li>" + answersArray[3] + 
+		"</li><li>" + answersArray[4] + "</li><li>" + answersArray[5] + "</li></ol></td></tr></table>"
 		show('resultsDisplay');
 }else{
 		hide('answers');
 		hide('score');
-		// document.getElementById('resultsDisplay').innerHTML = "<table><tr><td>Correct Answers</td></tr><tr>" +
-		// "<td><ol><li>World Wide Web</li><li>Octagon</li><li>Marie Curie</li><li>Tea</li><li>China</li><li>Volleyball</li></ol></td>" +
-		// "</tr><tr><td>Your Answers</td></tr><tr><td><ol><li>" + answersArray[0] + "</li><li>" + answersArray[1] + "</li><li>" + answersArray[2] + 
-		// "</li><li>" + answersArray[3] + "</li><li>" + answersArray[4] + "</li><li>" + answersArray[5] + "</li></ol></td></tr></table>"
+		document.getElementById('resultsDisplay').innerHTML = "<table><tr><td>Correct Answers</td></tr><tr>" +
+		"<td><ol><li>World Wide Web</li><li>Octagon</li><li>Marie Curie</li><li>Tea</li><li>China</li><li>Volleyball</li></ol></td>" +
+		"</tr><tr><td>Your Answers</td></tr><tr><td><ol><li>" + answersArray[0] + "</li><li>" + answersArray[1] + "</li><li>" + answersArray[2] + 
+		"</li><li>" + answersArray[3] + "</li><li>" + answersArray[4] + "</li><li>" + answersArray[5] + "</li></ol></td></tr></table>"
 		show('resultsDisplay');
 }
 };
