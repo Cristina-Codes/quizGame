277f474df770f7b4e3f4cf95f30ac667dfbe30fe
CristinaGrosser <cmgrosser@comcast.net>
2022-04-13 15:21:50 -0700

    Updated code from start through the 6 questions

--- scripts.js
+++ scripts.js
@@ -1,174 +1,105 @@
+const quiz = {};
+
+quiz.questions = [
+	'What does “www” stand for in a website browser?',
+	'What geometric shape is generally used for stop signs?',
+	'Who was the first woman to win a Nobel Prize (in 1903)?',
+	'What is the most consumed manufactured drink in the world?',
+	'Which country invented ice cream?',
+	'Dump, floater, and wipe are terms used in which team sport?'
+];
+
 // correct answers in lower case to compare strings to player answers
-const correctAnswers = ["World Wide Web", "Octagon", "Marie Curie", "Tea", "China", "Volleyball"];
+quiz.correctAnswers = ["World Wide Web", "Octagon", "Marie Curie", "Tea", "China", "Volleyball"];
 
 // empty array to push player answers into
-let answersArray = [];
+quiz.answersArray = [];
+quiz.actualScore = 0;
+quiz.clickCounter = 0;
 
-let actualScore = 0;
+// querying elements
+quiz.startGame = document.querySelector("#startGame");
+quiz.submitAnswer = document.querySelector("#submitAnswer");
+quiz.questionDisplay = document.querySelector("#questions");
+quiz.answers = document.querySelector('input');
 
-let clickCounter=0;
 
+// gameplay controls
+quiz.startGame.addEventListener('click', (e) => {
+	e.preventDefault();
+	quiz.startGame.classList.toggle('hidden');
+	quiz.submitAnswer.classList.toggle('hidden');
+	quiz.answers.removeAttribute('disabled');
+	quiz.questionDisplay.innerHTML = `${quiz.questions[quiz.clickCounter]}`;
+	quiz.clickCounter++;
+})
 
-
-
-// Controls for gameplay
-document.getElementById("button").onclick=function() {
-	clickCounter++;
-	if (clickCounter == 1) {
-		document.getElementById("button").innerHTML = "Final Answer";
-		hide("guess");
-		hide("doya");
-		show("q1");
-		show("guess1");
+quiz.submitAnswer.addEventListener('click', (e) => {
+	e.preventDefault();
+	if(quiz.clickCounter < 6){
+		const indexNum = quiz.clickCounter;
+		quiz.questionDisplay.innerHTML = `${quiz.questions[indexNum]}`;
+		quiz.clickCounter++;
+		//run the value through the titlecase function
+		quiz.titleCase(quiz.answers.value);
+		quiz.answers.value = "";
+	} else {
+		quiz.answers.classList.add('hidden'); //not
+		quiz.submitAnswer.classList.add('hidden'); //not
+		console.log("woohoo"); //working
 	}
+})
 
-	if (clickCounter == 2) {
-		answers("guess1");
-		hide("q1");
-		hide("guess1");
-		show("q2");
-		show("guess2")
-	}
-
-	if (clickCounter == 3) {
-		answers("guess2");
-		hide("q2");
-		hide("guess2");
-		show("q3");
-		show("guess3");
-	}
-
-	if (clickCounter == 4) {
-		answers("guess3");
-		hide("q3");
-		hide("guess3");
-		show("q4");
-		show("guess4");
-	}
-
-	if (clickCounter == 5) {
-		answers("guess4");
-		hide("q4");
-		hide("guess4");
-		show("q5");
-		show("guess5");
-	}
-
-	if (clickCounter == 6) {
-		answers("guess5");
-		hide("q5");
-		hide("guess5");
-		show("q6");
-		show("guess6");
-	}
-
-
-	if (clickCounter == 7) {
-		answers("guess6");
-		scoreTracker("guess6");
-		hide("q6");
-		hide("guess6");
-		hide('questionsContainer');
-		hide('button');
-		show("score");
-		show('answers');
-		document.getElementById("score").innerHTML = `<p class='endScreen'>Whoa!</p><p>Your score is:  ${actualScore}/6</p>`;
-  }
-}
 
 // Display player/correct answers - the display changes based on screen size
-document.getElementById("answers").onclick=function() {
-	if($(window).width() > 767) {
-		hide('answers');
-		hide('score');
-		document.getElementById('resultsDisplay').innerHTML = `
-		<table>
-			<tr>
-				<thead>
-					<td>Correct Answers</td>
-					<td>Your Answers</td>
-				</thead>
-			</tr>
-			<tr>
-				<td>
-					<ol>
-						<li>World Wide Web</li>
-						<li>Octagon</li>
-						<li>Marie Curie</li>
-						<li>Tea</li>
-						<li>China</li>
-						<li>Volleyball</li>
-					</ol>
-				</td>
-				<td>
-					<ol>
-						<li>${answersArray[0]}</li>
-						<li>${answersArray[1]}</li>
-						<li>${answersArray[2]}</li>
-						<li>${answersArray[3]}</li>
-						<li>${answersArray[4]}</li>
-						<li>${answersArray[5]}</li>
-					</ol>
-				</td>
-			</tr>
-		</table>`
-		show('resultsDisplay');
-}else{
-		hide('answers');
-		hide('score');
-		document.getElementById('resultsDisplay').innerHTML = `
-		<table>
-			<tr>
-				<td>Correct Answers</td>
-			</tr>
-			<tr>
-				<td>
-					<ol>
-						<li>World Wide Web</li>
-						<li>Octagon</li>
-						<li>Marie Curie</li>
-						<li>Tea</li>
-						<li>China</li>
-						<li>Volleyball</li>
-					</ol>
-				</td>
-			</tr>
-			<tr>
-				<td>Your Answers</td>
-			</tr>
-			<tr>
-				<td>
-					<ol>
-						<li>${answersArray[0]}</li>
-						<li>${answersArray[1]}</li>
-						<li>${answersArray[2]}</li>
-						<li>${answersArray[3]}</li>
-						<li>${answersArray[4]}</li>
-						<li>${answersArray[5]}</li>
-					</ol>
-				</td>
-			</tr>
-		</table>`  //rework using to-do app logic - createElement, appendChild
-		show('resultsDisplay');
-}
-};
+// document.getElementById("answers").onclick=function() {
+// 	if($(window).width() > 767) {
+// 		hide('answers');
+// 		hide('score');
+// 		document.getElementById('resultsDisplay').innerHTML = `
+// 		<table>
+// 			<tr>
+// 				<thead>
+// 					<td>Correct Answers</td>
+// 					<td>Your Answers</td>
+// 				</thead>
+// 			</tr>
+// 			<tr>
+// 				<td>
+// 					<ol>
+// 						<li>${correctAnswers[0]}</li>
+// 						<li>${correctAnswers[1]}</li>
+// 						<li>${correctAnswers[2]}</li>
+// 						<li>${correctAnswers[3]}</li>
+// 						<li>${correctAnswers[4]}</li>
+// 						<li>${correctAnswers[5]}</li>
+// 					</ol>
+// 				</td>
+// 				<td>
+// 					<ol>
+// 						<li>${answersArray[0]}</li>
+// 						<li>${answersArray[1]}</li>
+// 						<li>${answersArray[2]}</li>
+// 						<li>${answersArray[3]}</li>
+// 						<li>${answersArray[4]}</li>
+// 						<li>${answersArray[5]}</li>
+// 					</ol>
+// 				</td>
+// 			</tr>
+// 		</table>`
+// 		show('resultsDisplay');
+// 	}
+// };
 
-// Hide/show elements
-function hide(Id) {
-document.getElementById(Id).style.display = "none";
+
+// titleCase and push to answersArray
+quiz.titleCase = (answerValue) => {
+	const newValue = answerValue.toLowerCase().split(' ').map((word) => {
+		return word.replace(word[0], word[0].toUpperCase());
+	}).join(' ');
+	quiz.answersArray.push(newValue);
 }
 
-function show(Id){
-document.getElementById(Id).style.display ="block";
-}
-
-// Push player answers to array
-function answers(Id){
-let temp = document.getElementById(Id).value.toLowerCase().split(' ').map(function(word) {
-    return word.replace(word[0], word[0].toUpperCase());
-  }).join(' ');
-answersArray.push(temp);
-}
 
 // Find the score
 function scoreTracker(Id){
